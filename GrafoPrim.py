2578cd8a7d6088f787f9f32ace1eca20f34f60d1
AlanCruzCh <alan.francisco.cruz.chavez.17@gmail.com>
2022-06-14 00:33:28 -0500

    La implementacion del algortimo prim ya esta completa, se tuvo que agregar un programa mas el cual es GrafoPrim.py para poder ver como es el grafo antes de aplicar el algortmo

--- GrafoPrim.py
+++ GrafoPrim.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 # crear un grafo para la red optimizada
 G = nx.Graph() 
 
-# crea el grafo para la red antes de optimizar
-G2 = nx.Graph()
-
 # Añade nodos al grafo G
 def añadir_nodo(G, nodo):
     G.add_node(nodo)
@@ -86,101 +83,15 @@ grafo = {
         'BI1' : [('PO2',37),('DO1',105)]
 		}
 
-##     CREAR LAS ESTRUCTURAS DE DATOS NECESARIAS PARA ALMACENAR
-##     LOS DATOS NECESARIOS PARA EL ALGORITMO DE PRIM.
-##     EN PYTHON ES MUY FÁCIL CREAR LISTAS Y DICCIONARIOS NECESARIOS
-##     COMO SE MUESTRA A CONTINUACIÓN
-listaVisitados = []
-grafoResultante = {}
-listaOrdenada = []
+#Añadimos los nodos al grafo
+for key in grafo:
+    nodo_origen = key
+    añadir_nodo(G=G, nodo=nodo_origen)
 
-#    COMIENZO DEL ALGORITMO DE PRIM
-#1.- ELEGIR NODO ORIGEN AL AZAR O PEDIRLO AL USUARIO
-origen = input("\nIngresa el nodo origen: ")
-
-#2.- AGREGARLO A LA LISTA DE VISITADOS
-listaVisitados.append(origen)
-
-#3.- AGREGAR SUS ADYACENTES A LA LISTA ORDENADA
-for destino, peso in grafo[origen]:
-  listaOrdenada.append((origen, destino, peso))
-
-'''ORDENAMIENTO INSERT PARA LA LISTA'''
-listaOrdenada = [(c,a,b) for a,b,c in listaOrdenada]
-listaOrdenada.sort()
-listaOrdenada = [(a,b,c) for c,a,b in listaOrdenada]
-
-
-#4.- MIENTRAS LA LISTA ORDENADA NO ESTE VACIA, HACER:
-while listaOrdenada:
-
-  #5.-TOMAR VERTICE DE LA LISTA ORDENADA Y ELIMINARLO
-  vertice = listaOrdenada.pop(0)
-  d = vertice[1]
-
-  #6.-SI EL DESTINO NO ESTA EN LA LISTA DE VISITADOS
-  if d not in listaVisitados:
-    
-    #7.- AGREGAR A LA LISTA DE VISITADOS EN NODO DESTINO
-    listaVisitados.append(d)
-    
-    #8.- AGREGAR A LA LISTA ORDENADA LOS ADYACENTES DEL NODO DESTINO 
-    #"d" QUE NO HAN SIDO VISITADOS
-    for key, lista in grafo[d]:
-      if key not in listaVisitados:
-        listaOrdenada.append((d, key, lista))
-    
-    #####ORDENAMIENTO APLICADO A LA LISTA :
-    listaOrdenada = [(c,a,b) for a,b,c in listaOrdenada]
-    listaOrdenada.sort()
-    listaOrdenada = [(a,b,c) for c,a,b in listaOrdenada]
-
-#9.-AGREGAR VERTICE AL GRAFO RESULTANTE
-    # PARA COMPRENDER MEJOR, EN LAS SIGUIENTES LINEAS SE TOMA EL "VERTICE", QUE EN ESTE CASO
-    # ES UNA TUPLA QUE CONTIENE TRES VALORES; EL VERTICE EN SU POSICIÓN 0 ES EL VALOR DEL NODO ORIGEN
-    # EL VÉRTICE EN SU POSICIÓN 1 ES EL NODO DESTINO, Y EL VÉRTICE EN SU POSICIÓN 2 ES EL PESO DE LA ARISTA ENTRE AMBOS NODOS,
-    # Y A CONTINUACIÓN SE AGREGAN ESOS VALORES AL GRAFO
-    origen  = vertice[0]
-    destino = vertice[1]
-    peso    = vertice[2]
-
-    if origen in grafoResultante:
-        if destino in grafoResultante:
-            lista = grafoResultante[origen]
-            grafoResultante[origen] = lista + [(destino, peso)]
-            lista = grafoResultante[destino]
-            lista.append((origen, peso))
-            grafoResultante[destino] = lista
-            añadir_conexion(G=G, nodo_origen=origen, nodo_destino=destino, peso=peso)
-    
-        else:
-            grafoResultante[destino] = [(origen, peso)]
-            lista = grafoResultante[origen]
-            lista.append((destino, peso))
-            grafoResultante[origen] = lista
-            añadir_nodo(G=G, nodo=destino)
-            añadir_conexion(G=G, nodo_origen=origen, nodo_destino=destino, peso=peso)
-
-    elif destino in grafoResultante:
-        grafoResultante[origen] = [(destino, peso)]
-        lista = grafoResultante [destino]
-        lista.append((origen, peso))
-        grafoResultante[destino] = lista
-        añadir_nodo(G=G, nodo=origen)
-        añadir_conexion(G=G, nodo_origen=origen, nodo_destino=destino, peso=peso)
-
-    else:
-        grafoResultante[destino] = [(origen, peso)]
-        grafoResultante[origen] = [(destino, peso)]
-        añadir_nodo(G=G, nodo=origen)
-        añadir_nodo(G=G, nodo=destino)
-        añadir_conexion(G=G, nodo_origen=origen, nodo_destino=destino, peso=peso)
-
-
-print("\n\nGrafo resultante:\n")
-for key, lista in grafoResultante.items():
-  print(key)
-  print(lista)
+#Añadimos las conexions con los pesos de cada una de estas
+for nodo_origen in grafo:
+    for nodo_destino, peso in grafo[nodo_origen]:
+        añadir_conexion(G=G, nodo_origen=nodo_origen, nodo_destino=nodo_destino, peso=peso)
 
 
 # Mostramos el grafo resultante
@@ -190,4 +101,3 @@ labels = nx.get_edge_attributes(G, "weigth")
 nx.draw_networkx_edge_labels(G, pos, edge_labels=labels)
 
 plt.show()
-
